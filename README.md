2263f420e84dd3f3f5e42138fee86591f0b2f265
JoaoLace <joaopaulolacerdaleao@gmail.com>
2023-09-23 19:40:32 -0300

    Leia o readme

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # FinalProject
+
+(x) = Problema
+(1) = Problema 1
+
+!! Mudanças (JP) - 23/09 - 19:23 !!
+    -> Geral: 
+        Mudei a forma como o cadastro funciona:
+            - Quando o user clicar em cadastrar:
+                * É criado um novo campo de input 
+                * Esse novo campo é identico aos outros
+                * Muda o texto do titulo
+                * Muda o texto do butao
+    -> Html
+        * Adicionei id ao texto do titulo (pra poder muda-lo)
+        * Adicionei id ao botao (pra poder muda-lo)
+        * Adicionei um onclick ao texto de cadastro
+
+    -> Css
+        * Ao passar o mouse no texto de cadastrar o cursor muda
+        (2)
+    -> JavaScript
+        Funções: 
+            - criarCampoInput ()
+                * Essa função leva alguns parametros (pra criar um clone dos outros campos de input)
+                * Retorna um novo campo de input
+            - cadastrar()
+                * Cria uma variavel com a função anterior
+                * Muda o texto do titulo
+                * Muda o texto do butao
+                * Aumenta o tamanho do negocio la, pra poder suportar o novo elemento
+                * Cria o novo input antes do input de email
+                * (1) Muda a propriedade do butao (onclick)
+            - registrarConta()
+                * ...
+
+    -> Problemas
+        (1) - O botão não esta funcionando direito, ele nao chama a função que deveria chamar (registrarConta()), provavelmente porque:
+                * Nao é realmente um botão, é um input type submit
+                    eu ACHO que o submit meio que reescreve por cima da propriedade onclick
+            - Nao mudei já que nao queria mudar nada no html e css  
+                saber o que vc (Davi) acha, ja que foi vc que fez tudo
+        
+        (2) - Nao é beeemm um problema, é so que vc deixou um espaço vazio na linha 110 
+            (
+                form a:hover {
+                  color: ;
+                            }
+            )
---
+++ scrpits.js
@@ -0,0 +1,76 @@
+
+function criarCampoInput(id, inputClass, labelFor, labelText, isRequired) {
+    
+    /*
+        -Aceita os parametros pra criar um input identico aos outros
+        -Cria uma nova area de input para o usuario conseguir 
+        criar sua conta
+        - (Acho melhor doq mandar para uma nova pagina)
+    */
+    
+     // Seleciona o elemento de titulo da pagina (Login ou Cadastro)
+    let newText = document.getElementById("title")
+    // Muda o conteudo de texto 
+    newText.innerText = "Cadastro"
+    
+     // Criar variavel para o novo elemento input (durrhhh)
+    var divInputNome = document.createElement("div");
+    divInputNome.classList.add("input-single");
+
+    // Cria o elemento input
+    var inputElement = document.createElement("input");
+    inputElement.setAttribute("type", "text");
+    inputElement.id = id;
+    inputElement.classList.add("input", inputClass);
+
+
+    //  Setar o atributo de requerimento
+    if (isRequired) {
+        inputElement.setAttribute("required", "required");
+    }
+
+    // Cria o label
+    var labelElement = document.createElement("label");
+    labelElement.setAttribute("for", id);
+    labelElement.innerText = labelText;
+
+    // Adiciona o input e o label 
+    divInputNome.appendChild(inputElement);
+    divInputNome.appendChild(labelElement);
+
+    // Retorna a div criada
+    return divInputNome;
+}
+
+function cadastrar()
+{
+
+// chama a função
+
+var novoCampo = criarCampoInput("input-nome", "input", "input", "Nome", true);
+
+// Colocar o elemento <input> no lugar certo (antes do email)
+ var position = document.querySelector(".input-email")
+ position.parentNode.insertBefore(novoCampo, position)
+
+// Tira o texto de castrar
+
+var cadastrarTexto = document.getElementById("cadatrar-text")
+cadastrarTexto.innerText = ""
+
+ // Aumentar o tamanho do container para suportar os novos elementos
+ var container = document.getElementById("formulario")
+ container.style.height = "520px"
+
+ // Muda umas propriedades do butao
+ var butao = document.getElementById("butao")
+ butao.setAttribute("value","Criar conta")
+ butao.setAttribute("onclick","registrarConta")
+
+
+}
+
+function registrarConta()
+{
+    alert("penis")
+}
